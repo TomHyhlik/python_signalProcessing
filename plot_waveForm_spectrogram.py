17116275f4bbea815afbceb6dd74d8be8e727d8a
tomas.hyhlik <tomas.hyhlik@tirecheck.com>
2021-11-14 18:34:31 +0100

    add plot_waveForm_spectrogram.py and wav sample

---
+++ plot_waveForm_spectrogram.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+from pylab import *
+import wave
+
+def show_wave_n_spec(speech):
+    spf = wave.open(speech,'r')
+    sound_info = spf.readframes(-1)
+    sound_info = fromstring(sound_info, 'int32')
+
+    f = spf.getframerate()
+
+    subplot(211)
+    plot(sound_info)
+    title('Wave from and spectrogram of %s' % sys.argv[1])
+
+    subplot(212)
+    spectrogram = specgram(sound_info, Fs = f, scale_by_freq=True,sides='default')
+
+    show()
+    spf.close()
+
+fil = sys.argv[1]
+
+show_wave_n_spec(fil)
+
